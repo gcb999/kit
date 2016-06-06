0e4aadf43000230574d7f3339e068a8cd99375e8
gcb999 <546611623@qq.com>
2016-06-06 18:13:47 +0800

    Initial Commit

---
+++ kit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kit
+//
+//  Created by gcb999 on 16/6/6.
+//  Copyright © 2016年 com.sailvan.gcb999. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kit
+//
+//  Created by gcb999 on 16/6/6.
+//  Copyright © 2016年 com.sailvan.gcb999. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
